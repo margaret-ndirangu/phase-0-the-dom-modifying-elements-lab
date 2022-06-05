8aaf8f0dadad537b7020f0b75c272bc96d346d49
Margaret <wanjirundirangu43@gmail.com>
2022-06-05 09:02:16 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.querySelector('#main').remove()
+console.log(document.querySelector(`main`));
+
+const newHeader= document.createElement("h1")
+newHeader.id="victory"
+//victory.innerHTML="Margie is the champion"
+newHeader.innerHTML = "Margie is the champion!";
+main.appendChild(victory)
+//console.log(victory);
+//main.remove();
+
+
